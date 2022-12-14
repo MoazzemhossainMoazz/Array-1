196377f3aa42207532b8fe02a39fedfd38bfc413
Moazz <moazzemh222@gmail.com>
2022-12-14 23:12:18 +0600

    Array-2

--- Program.cs
+++ Program.cs
@@ -11,12 +11,22 @@ namespace Array_1
     {
         static void Main(string[] args)
         {
-            string[] cars = { "BMW", "Volvo", "Ford" };
-            cars[0] = "Toyota";
-            Console.WriteLine(cars[0]);
+        //    string[] cars = { "BMW", "Volvo", "Ford" };
+        //    cars[0] = "Toyota";
+        //    Console.WriteLine(cars[0]);
 
-                int[] numbers = { 23, 25, 45, 67, 56 };
-            Console.WriteLine(numbers[4]);
+        //        int[] numbers = { 23, 25, 45, 67, 56 };
+        //    Console.WriteLine(numbers[4]);
+        //    Console.ReadLine();
+
+        string firstName = "Alex";
+        string lastName = "Doolan";
+
+        string myFullName = string.Concat(firstName, lastName);
+        Console.WriteLine(myFullName);
+
+            Console.WriteLine(firstName.IndexOf("x"));
+            Console.WriteLine(firstName[3]);
             Console.ReadLine();
         }
     }
