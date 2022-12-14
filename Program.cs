7029a7e719ed3448a9cf2eb2d9f375c3d68d0eee
Moazz <moazzemh222@gmail.com>
2022-12-14 22:22:53 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "BMW", "Volvo", "Ford" };
+            cars[0] = "Toyota";
+            Console.WriteLine(cars[0]);
+
+                int[] numbers = { 23, 25, 45, 67, 56 };
+            Console.WriteLine(numbers[4]);
+            Console.ReadLine();
+        }
+    }
+}
